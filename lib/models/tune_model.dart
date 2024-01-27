3475e83b39be1053ae7147b671da7ffe50014082
amratef22 <atefamr040@gmail.com>
2024-01-27 21:16:48 +0200

    first commit

---
+++ lib/models/tune_model.dart
@@ -0,0 +1,17 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/animation.dart';
+
+class TuneModel
+{
+  final Color color;
+  final String sound;
+
+ const  TuneModel({required this.color, required this.sound});
+
+  playSound()
+  {
+    final player=AudioPlayer();
+    player.play(AssetSource(sound));
+    print('tune played success');
+  }
+}
